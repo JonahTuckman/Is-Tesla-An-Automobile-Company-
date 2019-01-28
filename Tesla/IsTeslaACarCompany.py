eca7bf99c67ed1eebe9329cd0004040b677b2eb2
Jonah Tuckman <JonahTuckman@Jonahs-MacBook-Pro.local>
2019-01-28 17:46:05 -0500

    Added folder with Tesla and other car companies data pulled from Yahoo finance from the past year

---
+++ Tesla/IsTeslaACarCompany.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 28 16:05:32 2019
+
+@author: JonahTuckman
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+Tesla = pd.read_csv("TSLA.csv")
+
+
+Ford = pd.read_csv("F.csv")
+GeneralMotors = pd.read_csv("GM.csv")
+Honda = pd.read_csv("HMC.csv")
+Toyota = pd.read_csv("TM.csv")
