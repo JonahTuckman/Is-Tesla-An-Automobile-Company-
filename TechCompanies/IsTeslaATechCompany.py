a8ccef38d230d4743695ab6b069affb8083600c1
Jonah Tuckman <JonahTuckman@Jonahs-MacBook-Pro.local>
2019-01-29 13:25:30 -0500

    Added data sets and program to predict tesla falls or drops based on tech company portfolios

---
+++ TechCompanies/IsTeslaATechCompany.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 13:16:20 2019
+
+@author: JonahTuckman
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+DataSet = pd.read_csv("FullDataSetTech.csv")
+X = DataSet.iloc[:, 3:].values
+
+Y = DataSet.iloc[:, 2].values
+X_opt = X[:,[1,3,5,7,9]]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test = train_test_split(X_opt, Y, test_size = 0.2, random_state = 0)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, Y_train)
+
+y_prediction = regressor.predict(X_test)
+output= []
+for i in y_prediction:
+    output.append(np.sign(i))
+    
+# accuracy
+count = 0
+for i in range(50):
+    if Y_test[i] == output[i]:
+        count += 1
+
+percent = (count / 50) * 100
+print('%d Percent Accuracy' % percent)
+
+## Percentage is 70 Percent Accuracy
